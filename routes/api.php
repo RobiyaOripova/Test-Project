9b1f9f477a2b033010ef62fc0e179002a0df8b29
Robiya Oripova <robiya.o@tbcbank.uz>
2025-10-14 18:09:59 +0500

    Refactor task routes to use `apiResource` for cleaner and more concise routing definition

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,6 @@ Route::get('/', function () {
     return response()->json(['message' => 'Hello World!']);
 });
 
-Route::prefix('tasks')->middleware('auth.basic')->group(function () {
-    Route::get('/', [TaskController::class, 'index']);
-    Route::get('/{task}', [TaskController::class, 'show']);
-    Route::post('/', [TaskController::class, 'create']);
-    Route::put('/{task}', [TaskController::class, 'update']);
-    Route::delete('/{task}', [TaskController::class, 'delete']);
-});
+
+
+Route::middleware('auth.basic')->apiResource('tasks', TaskController::class);
